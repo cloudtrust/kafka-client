4734d2d262ce83d541bacdb6f57fcfcc32cd7470
Francis PEROT <francis.perot@elca.ch>
2024-03-20 18:45:46 +0100

    Small enhancements, add missing functions, ...

--- mappers/mappers.go
+++ mappers/mappers.go
@@ -5,7 +5,8 @@ import (
 	"encoding/base64"
 )
 
-func DecodeBase64Bytes(ctx context.Context, in interface{}) (interface{}, error) {
+// DecodeBase64Bytes is a mapper to convert a content from base64
+func DecodeBase64Bytes(ctx context.Context, in any) (any, error) {
 	var bytes = in.([]byte)
 	return base64.StdEncoding.DecodeString(string(bytes))
 }
